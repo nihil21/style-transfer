d03027464bb6deb5e87e6ba0b10eeb8d8da26e27
nihil <mattia.orlandi@studio.unibo.it>
2020-04-14 15:36:47 +0200

    Added README.md and sample images

--- style-transfer/model/style_content_processing.py
+++ style-transfer/model/style_content_processing.py
@@ -27,7 +27,7 @@ def gram_matrix(input_tensor):
 def tensor_to_image(tensor):
     tensor = tensor*255
     tensor = np.array(tensor, dtype=np.uint8)
-    if np.ndim(tensor)>3:
+    if np.ndim(tensor) > 3:
         assert tensor.shape[0] == 1
         tensor = tensor[0]
     return PIL.Image.fromarray(tensor)
